eb9c7a6ff458142df73e106be91997e79bfd5211
gabriele <pashkug@gmail.com>
2023-05-12 20:43:29 +0200

    changes

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,16 +5,28 @@ import "./Body.css"
 const  Body = () => {
     return (
         <div className = "body">
-            <img className = "africa-map" src = "./img/africa.jpg"/>
+            <img className = "africa-map" src = "../../../public/img/africa.jpg" alt="africa.jpg"/>
                 <div className = "row-food">
 
                     <div className = "africa-food">
-                
-                
+                        <p className = "food-title">
+                            Thypical African Food
+                        </p>
+                        <p className = "food-nav">
+                            Awesome raw zebra
+                        </p>
+                        <p className = "food-nav">
+                            Awesome raw niger
+                        </p>
+                        <p className = "food-nav">
+                            Awesome raw water
+                        </p>
                     </div>
                     
                 </div>
-
+            <div className = "curiosities">
+                <p> ciao </p>
+            </div>
             
         </div>
     )
